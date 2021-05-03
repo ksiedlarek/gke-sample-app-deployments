ba117d7c2225fa56395e081dc170ab7726f4bf9c
Kasia Siedlarek <kasia@siedlarek.net>
2021-05-03 15:24:33 +0200

    ci/cd

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,8 +1,3 @@
-output "bucket_uri" {
-  value       = google_container_registry.registry.bucket_self_link
-  description = "URI of created bucket"
-}
-
 output "lb_ip" {
   value = kubernetes_service.hello_app.status.0.load_balancer.0.ingress.0.ip
 }
