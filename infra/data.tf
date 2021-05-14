7d8a62098d1fb71feeb8105d59dc85ef4b8b38cd
k.siedlarek <k.siedlarek@accenture.com>
2021-05-14 11:02:15 +0200

    cleanup

--- infra/data.tf
+++ infra/data.tf
@@ -1,18 +1,6 @@
-# data "terraform_remote_state" "gke" {
-#   backend = "remote"
-#   config = {
-#     organization = "gcp-demo-ksiedlarek"
-#     workspaces = {
-#       name = "gke-base-infra"
-#     }
-#   }
-# }
-
 data "google_client_config" "default" {}
 
 data "google_container_cluster" "my_cluster" {
-  # name     = data.terraform_remote_state.gke.outputs.kubernetes_cluster_name
-  # location = "europe-central2-a"
   name     = var.gke_cluster_name
   location = var.zone
 }
